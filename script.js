971ec59740c74a5c3234138b255d0ec4af3f7ecc
Ines Delgado <inesdelgadohernandez1977@gmail.com>
2023-06-15 09:09:32 +0100

    buscar todos

--- script.js
+++ script.js
@@ -29,6 +29,42 @@ function buscarRandomCocktail() {
         .then((data) => mostrarData(data));
 }
 
+document.getElementById('todos-boton').addEventListener('click', buscarTodosCocktail);
+
+function buscarTodosCocktail() {
+    fetch('https://inesdelgadohernandez.somee.com/api/cocktail/todos',
+        {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => mostrarTodosCocktail(data));
+}
+
+function mostrarTodosCocktail(data) {
+    console.log(data);
+    let container = document.getElementById('container');
+
+    data.forEach(cocktail => {
+        let nombreCocktail = document.createElement('h1');
+        nombreCocktail.innerHTML = cocktail.nombre;
+        container.appendChild(nombreCocktail);
+        nombreCocktail.className = 'nombre-cocktail';
+
+        let receta = document.createElement('p');
+        receta.innerHTML = cocktail.receta;
+        container.appendChild(receta);
+        receta.className = 'receta-cocktail';
+
+        let foto = document.createElement('img');
+        foto.src = cocktail.fotoSrc;
+        container.appendChild(foto);
+        foto.className = 'foto-cocktail';
+    });
+}
+
 function mostrarData(data) {
     console.log(data);
 
